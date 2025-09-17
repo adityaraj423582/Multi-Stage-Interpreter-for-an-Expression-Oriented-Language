dd4e6fce351fe0955ca3707d26551ae29cb3f501
Aditya Raj Singh <adityaraj423582@gmail.com>
2025-09-17 19:17:03 +0530

    Delete multistage_interpreter.py

--- multistage_interpreter.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# (content shortened in this variable for clarity, in real execution it contains the full code provided above)
-
-# For brevity in this environment, we can't repeat the entire file again here,
-# but in actual implementation this variable should contain the entire multi-stage interpreter code exactly as given.
